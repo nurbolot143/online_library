e647fcfc3a46f28bfd311618a5cea8bb4a4aa994
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-05-30 17:04:05 +0300

    added route, initial components and pages

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__logo">
+            <Link to="/">Online Library</Link>
+          </div>
+
+          <ul className="nav__list">
+            <li className="nav__item">
+              <NavLink
+                to="/"
+                style={({ isActive }) =>
+                  isActive ? { color: "#fcc349" } : undefined
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/wishList"
+                style={({ isActive }) =>
+                  isActive ? { color: "#fcc349" } : undefined
+                }
+              >
+                Wish List
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
