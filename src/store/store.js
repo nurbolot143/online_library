c1ac53adb7ab2b23d560351a4029ebceb5cb2c18
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-06-01 12:56:17 +0300

    fixed bug

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import books from "../components/booksList/booksSlice";
 import wishList from "../components/bookWishList/wishListSlice";
 import filters from "../components/filters/filtersSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     books,
     wishList,
     filters,
